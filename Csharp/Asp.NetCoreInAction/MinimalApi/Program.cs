464da53bc4d116c8fd8d29aaf7bcf23316cd6823
lizan <xsf9728@gmail.com>
2023-08-21 22:48:54 +0800

    ASP.NET Core in Action: MinimalApi, add simple CRUD for fruits

--- Csharp/Asp.NetCoreInAction/MinimalApi/Program.cs
+++ Csharp/Asp.NetCoreInAction/MinimalApi/Program.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Net.Mime;
 using MinimalApi.Data;
 using static HelperUtils.ColoredLog;
 
@@ -12,6 +13,40 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePages();
 
+// Test with fruits.
+var _fruits = new ConcurrentDictionary<string, Fruit>()
+{
+    ["Apple"] = new("Apple", 10),
+    ["Pear"] = new("Pear", 5),
+    ["Peach"] = new("Peach", 3),
+};
+app.MapGet("/fruit", () => _fruits);
+app.MapGet("/fruit/{id}", (string id) =>
+    _fruits.TryGetValue(id, out Fruit? fruit) ? TypedResults.Ok(fruit) : Results.NotFound()
+);
+app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+    _fruits.TryAdd(id, fruit) ? TypedResults.Created($"/fruit/{id}")
+        : Results.BadRequest(new { id = "A fruit with this id already exists."})
+);
+app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+{
+    _fruits[id] = fruit;
+    return Results.NoContent();
+});
+app.MapDelete("/fruit/{id}", (string id) =>
+{
+    _fruits.TryRemove(id, out _);
+    return Results.NoContent();
+});
+
+// Test with HttpResponse Handler
+app.MapGet("/teapot", (HttpResponse response) =>
+{
+    response.StatusCode = 200;
+    response.ContentType = MediaTypeNames.Text.Plain;
+    return response.WriteAsync("You're requesting Teapot!");
+});
+
 // app.MapGet("/", void () => throw new Exception("Test ExceptionHandler!")); // Test ProblemDetails in ExceptionHandler
 // app.MapGet("/", () => Results.NotFound()); // Test ProblemDetails for error status code pages
 app.MapGet("/", () => "Hello World!");
@@ -19,9 +54,3 @@ app.MapGet("/person/{name?}", (string? name) => $"Hello, {(name ?? "Mr. Anonymou
 
 app.Run();
 
-var _fruits = new ConcurrentDictionary<string, Fruit>()
-{
-    ["Apple"] = new("Apple", 10),
-    ["Pear"] = new("Pear", 5),
-    ["Peach"] = new("Peach", 3),
-};
