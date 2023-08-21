81a95bb947256ed35c81fa521a4674affa770de3
lizan <xsf9728@gmail.com>
2023-08-21 09:08:06 +0800

    ASP.NET Core in Action: MinimalApi, add problem details for status code pages and exceptions

--- Csharp/Asp.NetCoreInAction/MinimalApi/Program.cs
+++ Csharp/Asp.NetCoreInAction/MinimalApi/Program.cs
@@ -1,9 +1,21 @@
+using System.Collections.Concurrent;
+using MinimalApi.Data;
 using static HelperUtils.ColoredLog;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddProblemDetails();
 
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+app.UseStatusCodePages();
+
+// app.MapGet("/", void () => throw new Exception("Test ExceptionHandler!")); // Test ProblemDetails in ExceptionHandler
+// app.MapGet("/", () => Results.NotFound()); // Test ProblemDetails for error status code pages
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/person/{name?}", (string? name) => $"Hello, {(name ?? "Mr. Anonymous")}!");
 
 app.Run();
+
