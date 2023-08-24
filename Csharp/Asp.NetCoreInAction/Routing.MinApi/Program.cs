cda06e0b2d023f872f901231d655bb075a6de2fd
lizan <xsf9728@gmail.com>
2023-08-24 08:15:12 +0800

    ASP.NET Core in Action: Routing.MinApi, tune path style using RouteOptions and LinkOptions

--- Csharp/Asp.NetCoreInAction/Routing.MinApi/Program.cs
+++ Csharp/Asp.NetCoreInAction/Routing.MinApi/Program.cs
@@ -1,14 +1,25 @@
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<RouteOptions>(opt =>
+{
+    opt.LowercaseUrls = true;
+    opt.LowercaseQueryStrings = false;
+    opt.AppendTrailingSlash = false;
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/greeting/{name}", (string name) => $"Hello, {name}").WithName("greeting");
-app.MapGet("/greet/{name}", (string name, LinkGenerator links, HttpContext context) =>
+app.MapGet("/Greeting/{name}", (string name) => $"Hello, {name}").WithName("greeting");
+app.MapGet("/Greet/{name}", (string name, LinkGenerator links, HttpContext context) =>
 {
-    string? link = links.GetPathByName("greeting", new { name });
+    string? link = links.GetPathByName("greeting", new { name },
+        options: new LinkOptions{ AppendTrailingSlash = true }); // This will override RouteOptions
     string? uri = links.GetUriByName(context, "greeting", new { name });
     return $"Go to greeting page at path: {link}\nUri: {uri}";
+});
+app.MapGet("/redirect", () =>
+{
+    return Results.RedirectToRoute("greeting", new{ name = "Mr. Anonymous" });
 });
 
 app.Run();
