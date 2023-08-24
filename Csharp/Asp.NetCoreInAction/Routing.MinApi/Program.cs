f14e3c420633f76cc46a84d0f99c7a2411d8d888
lizan <xsf9728@gmail.com>
2023-08-24 08:53:56 +0800

    ASP.NET Core in Action: Routing.MinApi, route/query model binding a type which implements TryParse

--- Csharp/Asp.NetCoreInAction/Routing.MinApi/Program.cs
+++ Csharp/Asp.NetCoreInAction/Routing.MinApi/Program.cs
@@ -22,4 +22,24 @@ app.MapGet("/redirect", () =>
     return Results.RedirectToRoute("greeting", new{ name = "Mr. Anonymous" });
 });
 
+// Test module binding.
+app.MapGet("/product/{p}", (Product p) => $"Product of id {p.Id}");
+
 app.Run();
+
+// A type implementing TryParse can be used for model binding.
+readonly record struct Product(int Id)
+{
+    public static bool TryParse(string? input, out Product p)
+    {
+        if (input is not null
+            && input.StartsWith('p')
+            && int.TryParse(input.AsSpan()[1..], out int id))
+        {
+            p = new Product(id);
+            return true;
+        }
+        p = default;
+        return false;
+    }
+}
