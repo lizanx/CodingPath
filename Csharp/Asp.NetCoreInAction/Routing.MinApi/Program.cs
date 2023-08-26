6a2a6275fab461b791d42ef5500b4c53f27bbd3f
lizan <xsf9728@gmail.com>
2023-08-26 09:55:02 +0800

    ASP.NET Core in Action: Routing.MinApi, add array binding and customized binding

--- Csharp/Asp.NetCoreInAction/Routing.MinApi/Program.cs
+++ Csharp/Asp.NetCoreInAction/Routing.MinApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<RouteOptions>(opt =>
@@ -8,7 +9,7 @@ builder.Services.Configure<RouteOptions>(opt =>
     opt.AppendTrailingSlash = false;
 });
 // Configure Json serialization/deserialization options.
-builder.Services.ConfigureHttpJsonOptions((JsonOptions option) =>
+builder.Services.ConfigureHttpJsonOptions((Microsoft.AspNetCore.Http.Json.JsonOptions option) =>
 {
     option.SerializerOptions.AllowTrailingCommas = true;
     option.SerializerOptions.PropertyNameCaseInsensitive = true;
@@ -33,6 +34,12 @@ app.MapGet("/redirect", () =>
 // Test module binding.
 app.MapGet("/product/{p}", (Product p) => $"Product of id {p.Id}");
 
+// Test array bingding in query strings.
+app.MapGet("/array", ( [FromQuery(Name = "id")]int[] ids ) => $"Id count: {ids.Length}");
+
+// Test customized BindAsync.
+app.MapPost("/size", (SizeDetail size) => $"SizeDetail\nHeight: {size.Height}\nWidth: {size.Width}");
+
 app.Run();
 
 // A type implementing TryParse can be used for model binding.
@@ -49,5 +56,21 @@ readonly record struct Product(int Id)
         }
         p = default;
         return false;
+    }
+}
+
+record SizeDetail(double Height, double Width)
+{
+    public static async ValueTask<SizeDetail?> BindAsync(HttpContext context)
+    {
+        using var sr = new StreamReader(context.Request.Body);
+        string? line1 = await sr.ReadLineAsync(cancellationToken: context.RequestAborted);
+        if (line1 is null) return null;
+        string? line2 = await sr.ReadLineAsync(cancellationToken: context.RequestAborted);
+        if (line2 is null) return null;
+        return double.TryParse(line1, out double height)
+            && double.TryParse(line2, out double width)
+            ? new SizeDetail(height, width)
+            : null;
     }
 }
