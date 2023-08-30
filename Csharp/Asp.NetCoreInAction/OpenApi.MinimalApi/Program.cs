0d6cade870e9530671559f7ec85dafeaf565ef7e
lizan <xsf9728@gmail.com>
2023-08-30 15:31:35 +0800

    ASP.NET Core in Action: Update OpenApi.MinimalApi, use generated clients, fluent method to add metadata

--- Csharp/Asp.NetCoreInAction/OpenApi.MinimalApi/Program.cs
+++ Csharp/Asp.NetCoreInAction/OpenApi.MinimalApi/Program.cs
@@ -1,21 +1,26 @@
 using System.Collections.Concurrent;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opt =>
-    opt.SwaggerDoc(
-        "v1", new Microsoft.OpenApi.Models.OpenApiInfo()
-        {
-            Title = "Fruitify",
-            Description = "An API for interacting with fruit stock",
-            Version = "1.0"
-        }
-    )
-);
-
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(opt =>
+        opt.SwaggerDoc(
+            "v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+            {
+                Title = "Fruitify",
+                Description = "An API for interacting with fruit stock",
+                Version = "1.0"
+            }
+        )
+    );
+}
 var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
@@ -27,7 +32,14 @@ app.MapGet("/fruit/{id}", (string id) =>
     : Results.Problem(statusCode: 404)
 ).WithTags("fruit")
 .Produces<Fruit>()
-.ProducesProblem(statusCode: 404);
+.ProducesProblem(statusCode: 404)
+.WithSummary("Fetches a fruit")
+.WithDescription("Fetches a fruit by id, or returns 404 if no fruit has the id.")
+.WithOpenApi(op =>
+{
+    op.Parameters[0].Description = "The id of the fruit to fetch.";
+    return op;
+});
 app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
     _fruit.TryAdd(id, fruit)
     ? TypedResults.Created($"/fruit/{id}", fruit)
@@ -39,7 +51,16 @@ app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
     )
 ).WithTags("fruit")
 .Produces<Fruit>(statusCode: 201)
-.ProducesValidationProblem();
+.ProducesValidationProblem()
+// .WithSummary("Post a fruit")
+// .WithDescription("Create a fruit with an id, or returns 400 if id already exists.")
+.WithOpenApi(op =>
+{
+    op.Summary = "Post a fruit";
+    op.Description = "Create a fruit with an id, or returns 400 if id already exists.";
+    op.Parameters[0].Description = "The id of fruit to create.";
+    return op;
+});
 
 app.Run();
 
--- Csharp/Asp.NetCoreInAction/OpenApiClient.Console/Program.cs
+++ Csharp/Asp.NetCoreInAction/OpenApiClient.Console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fruit;
+
+var client = new FruitClient(
+    "http://localhost:5191",
+    new HttpClient()
+);
+Fruit.Fruit created = await client.FruitPOSTAsync(
+    "123",
+    new() { Name="Banana", Stock = 100 }
+);
+Console.WriteLine($"Fruit created: {created.Name}, {created.Stock}");
+Fruit.Fruit fetched = await client.FruitGETAsync("123");
+Console.WriteLine($"Fruit fetched: {fetched.Name}, {fetched.Stock}");
