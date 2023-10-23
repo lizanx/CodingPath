91f67e7200a7dbb2531a78e78ea2a2fcbb5bbbbf
lizan <xsf9728@gmail.com>
2023-10-23 09:15:41 +0800

    AppsAndServicesWithNet7[Chap02]: Add unit tests and do exercise 2.1

---
+++ Csharp/AppsAndServicesWithNet7/chap2/Exercise_2.1.md
@@ -0,0 +1,10 @@
+1. `Microsoft.Data.SqlClient`
+2. Create an instance of `SqlConnectionStringBuilder`, set its properties, then read its `ConnectionString` property.
+3. `@`
+4. Close the data reader.
+5. Generate data models from database, execute database migrations.
+6. `DbSet<T>`
+7. `ICollection<T>`
+8. `[ClassName]Id`
+9. Because it can do more than annotations, such as rows initialization.
+10. To cache some data which doesn't need to be included in database.
---
+++ Csharp/AppsAndServicesWithNet7/chap2/Northwind.Common.EntityModels.Tests/NorthwindEntityModelsTests.cs
@@ -0,0 +1,42 @@
+using Packt.Shared;
+
+namespace Northwind.Common.EntityModels.Tests;
+
+public class NorthwindEntityModelsTests
+{
+    [Fact]
+    public void CanConnectIsTrue()
+    {
+        using NorthwindContext db = new();
+        bool canConnect = db.Database.CanConnect();
+        Assert.True(canConnect);
+    }
+
+    [Fact]
+    public void ProviderIsSqlServer()
+    {
+        using NorthwindContext db = new();
+        string? provider = db.Database.ProviderName;
+        Assert.Equal("Microsoft.EntityFrameworkCore.SqlServer", provider);
+    }
+
+    [Fact]
+    public void ProductId1IsChai()
+    {
+        using NorthwindContext db = new();
+        Product product1 = db.Products.Single(p => p.ProductId == 1);
+        Assert.Equal("Chai", product1.ProductName);
+    }
+
+    [Fact]
+    public void EmployeeHasLastRefreshedIn10sWindow()
+    {
+        using NorthwindContext db = new();
+        Employee employee1 = db.Employees.Single(e => e.EmployeeId == 1);
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        Assert.InRange(
+            actual: employee1.LastRefreshed,
+            low: now.Subtract(TimeSpan.FromSeconds(5)),
+            high: now.AddSeconds(5));
+    }
+}
