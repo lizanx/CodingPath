3303c58629f953e1b52d9b2ef171dc69b3d65114
lizan <xsf9728@gmail.com>
2023-11-06 09:04:27 +0800

    AppsAndServicesWithNet7[Chap05]: Play with ImageSharp

---
+++ Csharp/AppsAndServicesWithNet7/chap5/WorkingWithImages/Program.cs
@@ -0,0 +1,39 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+string imagesFolder = Path.Combine(Environment.CurrentDirectory, "images");
+WriteLine("Looing for images in folder: {0}\n", imagesFolder);
+
+if (!Directory.Exists(imagesFolder))
+{
+    WriteLine("\nFolder doesn't exist!");
+    return;
+}
+
+IEnumerable<string> images = Directory.EnumerateFiles(imagesFolder);
+foreach (string imagePath in images)
+{
+    if (Path.GetFileNameWithoutExtension(imagePath).EndsWith("-thumbnail"))
+    {
+        WriteLine($"Skipping:\n  {imagePath}\n");
+        continue;
+    }
+
+    string thumbnailPath = Path.Combine(
+        imagesFolder,
+        Path.GetFileNameWithoutExtension(imagePath) + "-thumbnail"
+            + Path.GetExtension(imagePath)
+    );
+
+    using (Image image = Image.Load(imagePath))
+    {
+        WriteLine($"Converting:\n  {imagePath}");
+        WriteLine($"To:\n  {thumbnailPath}");
+        image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+        image.Mutate(x => x.Grayscale());
+        image.Save(thumbnailPath);
+        WriteLine();
+    }
+}
+
+WriteLine("Image processing complete. View the images folder.");
