9950533952e61d0781e11709a41daa565a7ccb22
lizan <xsf9728@gmail.com>
2023-11-08 09:30:43 +0800

    AppsAndServicesWithNet7[Chap05]: Play with FluentAssertion, FluentValidator and QuestPDF, finish Exercise5.1

---
+++ Csharp/AppsAndServicesWithNet7/chap5/Exercise_5.1.md
@@ -0,0 +1,10 @@
+1. Newtonsoft.Json
+2. `Mutate`
+3. It supports structured logging.
+4. Support for different output streams.
+5. No, Automapper is designed to map complicated models to simple models when they have common property names.
+6. `Should`
+7. `OnlyContain`
+8. `AbstractValidator<T>`
+9. Use the `When` function with a predicate.
+10. `Compose` and `GetMetadata` methods from `IDocument` interface.
---
+++ Csharp/AppsAndServicesWithNet7/chap5/GeneratingPdf.Application/Program.cs
@@ -0,0 +1,41 @@
+﻿using GeneratingPdf.Models;
+using GeneratingPdf.Document;
+using QuestPDF.Fluent;
+
+string filename = "catalog.pdf";
+
+Catalog model = new()
+{
+    Categories = new()
+    {
+        new() { CategoryId = 1, CategoryName = "Beverages"},
+        new() { CategoryId = 2, CategoryName = "Condiments"},
+        new() { CategoryId = 3, CategoryName = "Confections"},
+        new() { CategoryId = 4, CategoryName = "Dairy Products"},
+        new() { CategoryId = 5, CategoryName = "Grains/Cereals"},
+        new() { CategoryId = 6, CategoryName = "Meat/Poultry"},
+        new() { CategoryId = 7, CategoryName = "Produce"},
+        new() { CategoryId = 8, CategoryName = "Seafood"},
+    }
+};
+
+CatalogDocument document = new(model);
+document.GeneratePdf(filename);
+
+WriteLine($"PDF catalog has been created: {filename}");
+
+try
+{
+    if (OperatingSystem.IsWindows())
+    {
+        System.Diagnostics.Process.Start("explorer.exe", filename);
+    }
+    else
+    {
+        WriteLine("Open the file manually.");
+    }
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
