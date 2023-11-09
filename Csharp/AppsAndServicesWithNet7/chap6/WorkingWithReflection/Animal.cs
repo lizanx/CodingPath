6a3dce8329e0b440b3060c425542b5e83521d4fc
lizan <xsf9728@gmail.com>
2023-11-09 09:03:02 +0800

    AppsAndServicesWithNet7[Chap06]: Play with Assembly/Attribute/Type

---
+++ Csharp/AppsAndServicesWithNet7/chap6/WorkingWithReflection/Animal.cs
@@ -0,0 +1,11 @@
+namespace Packt.Shared;
+
+public class Animal
+{
+    [Coder("Mark Price", "22 August 2022")]
+    [Coder("Johnni Rasmussen", "13 September 2022")]
+    public void Speak()
+    {
+        WriteLine("Woof...");
+    }
+}
---
+++ Csharp/AppsAndServicesWithNet7/chap6/WorkingWithReflection/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using Packt.Shared;
+
+WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if (assembly is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+WriteLine($"\tFull Name: {assembly.FullName}");
+WriteLine($"\tLocation: {assembly.Location}");
+WriteLine($"\tEntry Point: {assembly.EntryPoint}");
+
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+WriteLine("\tAssembly-Level Attributes:");
+foreach (Attribute attribute in attributes)
+{
+    WriteLine($"\t\t{attribute.GetType()}");
+}
+
+AssemblyInformationalVersionAttribute? version =
+    assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+WriteLine($"** Version: {version?.InformationalVersion}");
+
+AssemblyCompanyAttribute? company =
+    assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
+WriteLine($"** Company: {company?.Company}");
+
+WriteLine();
+WriteLine("** Types:");
+Type[] types = assembly.GetTypes();
+
+foreach (Type type in types)
+{
+    WriteLine();
+    WriteLine($"Type: {type.Name}");
+    MemberInfo[] members = type.GetMembers();
+
+    foreach (MemberInfo member in members)
+    {
+        WriteLine("\t{0}: {1} ({2})",
+            member.MemberType, member.Name, member.DeclaringType?.Name);
+        
+        IOrderedEnumerable<CoderAttribute> coders =
+            member.GetCustomAttributes<CoderAttribute>()
+            .OrderByDescending(c => c.LastModified);
+        
+        foreach (CoderAttribute coder in coders)
+        {
+            WriteLine("\t-> Modified by {0} on {1}", coder.Coder, coder.LastModified);
+        }
+    }
+}
