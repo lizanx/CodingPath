14b07f1816443b787be2e85522354f2a5f2c7544
lizan <xsf9728@gmail.com>
2023-11-28 09:26:24 +0800

    AppsAndServicesWithNet7[Chap13]: Init SignalR projects

---
+++ Csharp/AppsAndServicesWithNet7/chap13/Northwind.Common/MessageModel.cs
@@ -0,0 +1,8 @@
+namespace Northwind.Chat.Models;
+
+public class MessageModel
+{
+    public string From { get; set; } = null!;
+    public string To { get; set; } = null!;
+    public string? Body { get; set; }
+}
