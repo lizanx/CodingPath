57a7d612aa099a5120b173145142db3d6acd3b7c
lizan <xsf9728@gmail.com>
2023-11-28 08:32:14 +0800

    AppsAndServicesWithNet7[Chap12]: AsyncUnaryCall<T> and gRPC Deadline

--- Csharp/AppsAndServicesWithNet7/chap12/Northwind.Grpc.Client.Mvc/Controllers/HomeController.cs
+++ Csharp/AppsAndServicesWithNet7/chap12/Northwind.Grpc.Client.Mvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Northwind.Grpc.Client.Mvc.Models;
 using Grpc.Net.Client; // GrpcChannel
 using Grpc.Net.ClientFactory; // GrpcClientFactory
+using Grpc.Core; // AsyncUnaryCall<T>
 
 namespace Northwind.Grpc.Client.Mvc.Controllers;
 
@@ -29,12 +30,33 @@ public class HomeController : Controller
             );
             ViewData["greeting"] = $"Greeting from grpc service: {reply.Message}";
 
-            ShipperReply shipperReply = await shipperClient.GetShipperAsync(
-                new ShipperRequest { ShipperId = id }
+            
+            // ShipperReply shipperReply = await shipperClient.GetShipperAsync(
+            //     new ShipperRequest { ShipperId = id }
+            // );
+
+            // the same call as above but not awaited            
+            AsyncUnaryCall<ShipperReply> shipperCall = shipperClient.GetShipperAsync(
+                new ShipperRequest { ShipperId = id },
+                deadline: DateTime.UtcNow.AddSeconds(3) // must be a UTC DateTime
             );
+            Metadata metadata = await shipperCall.ResponseHeadersAsync;
+            foreach (Metadata.Entry entry in metadata)
+            {
+                // not really critical, just doing this to make it easier to see
+                _logger.LogCritical($"Key: {entry.Key}, Value: {entry.Value}");
+            }
+            ShipperReply shipperReply = await shipperCall.ResponseAsync;
+
             ViewData["shipper"] = "Shipper from grpc services: " +
                 $"ID: {shipperReply.ShipperId}, Name: {shipperReply.CompanyName}, " +
                 $"Phone: {shipperReply.Phone}.";
+        }
+        catch (RpcException rpcex) when (rpcex.StatusCode ==
+            global::Grpc.Core.StatusCode.DeadlineExceeded)
+        {
+            _logger.LogWarning("Northwind.Grpc.Service deadline exceeded.");
+            ViewData["exception"] = rpcex.Message;
         }
         catch (Exception ex)
         {
