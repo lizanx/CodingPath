18e2529e967afc7f6b88096c7ad8c53a841ba760
lizan <xsf9728@gmail.com>
2023-12-07 09:03:41 +0800

    AppsAndServicesWithNet7[Chap16]:  Add Employee service and navigation menu

--- Csharp/AppsAndServicesWithNet7/chap16/Northwind.BlazorWasm/Server/Program.cs
+++ Csharp/AppsAndServicesWithNet7/chap16/Northwind.BlazorWasm/Server/Program.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Mvc; // [FromServices]
+using System.Text.Json.Serialization; // ReferenceHandler
+using Packt.Shared;
+
+// Define an alias for the JsonOptions class
+using HttpJsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<HttpJsonOptions>(options =>
+    {
+        // Handle circular references between employees and managers.
+        options.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddNorthwindContext();
 
 var app = builder.Build();
 
@@ -23,6 +36,36 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapGet("api/employees",
+    ([FromServices] NorthwindContext db) => Results.Json(db.Employees))
+    .WithName("GetEmployees")
+    .Produces<Employee[]>(StatusCodes.Status200OK);
+
+app.MapGet("api/employees/{id:int}",
+    ([FromServices] NorthwindContext db, [FromRoute] int id) =>
+        {
+            Employee? e = db.Employees.Find(id);
+            return e is null ? Results.NotFound() : Results.Json(e);
+        })
+    .WithName("GetEmployeesById")
+    .Produces<Employee>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
+
+app.MapGet("api/employees/{country}",
+    ([FromServices] NorthwindContext db, [FromRoute] string country) =>
+        Results.Json(db.Employees.Where(e => e.Country == country)))
+    .WithName("GetEmployeesByCountry")
+    .Produces<Employee[]>(StatusCodes.Status200OK);
+
+app.MapPost("api/employees",
+    async ([FromServices] NorthwindContext db, [FromBody] Employee employee) =>
+        {
+            db.Employees.Add(employee);
+            await db.SaveChangesAsync();
+            return Results.Created($"api/employees/{employee.EmployeeId}", employee);
+        })
+    .Produces<Employee>(StatusCodes.Status201Created);
 
 app.MapRazorPages();
 app.MapControllers();
