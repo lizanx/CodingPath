3a3038e771b1fec6371d767b04117f7e86544e68
lizan <xsf9728@gmail.com>
2023-12-11 09:33:36 +0800

    AppsAndServicesWithNet7[Chap17]: Add category service using Radzen

--- Csharp/AppsAndServicesWithNet7/chap17/Northwind.BlazorLibraries/Server/Program.cs
+++ Csharp/AppsAndServicesWithNet7/chap17/Northwind.BlazorLibraries/Server/Program.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Mvc; // [FromServices]
+using System.Text.Json.Serialization; // ReferenceHandler
+using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+using HttpJsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<HttpJsonOptions>(options =>
+    {
+        options.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddNorthwindContext();
 
 var app = builder.Build();
 
@@ -23,6 +36,20 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapGet("api/categories", ([FromServices] NorthwindContext db) =>
+        Results.Json(
+            db.Categories.Include(category => category.Products)
+        )
+    )
+    .WithName("GetCategories")
+    .Produces<Category[]>(StatusCodes.Status200OK);
+
+app.MapGet("api/orders/", ([FromServices] NorthwindContext db) =>
+        Results.Json(db.Orders.Include(order => order.OrderDetails))
+    )
+    .WithName("GetOrders")
+    .Produces<Order[]>(StatusCodes.Status200OK);
 
 app.MapRazorPages();
 app.MapControllers();
--- Csharp/AppsAndServicesWithNet7/chap17/Northwind.BlazorLibraries/Shared/SharedClass.cs
+++ Csharp/AppsAndServicesWithNet7/chap17/Northwind.BlazorLibraries/Shared/SharedClass.cs
@@ -1,1 +1,10 @@
-/* Shared classes can be referenced by both the Client and Server */
+namespace Packt.Shared;
+
+public static class NorthwindExtensionMethods
+{
+    public static string ConvertToBase64Jpeg(this byte[] picture)
+    {
+        return string.Format("data:image/jpg;base64,{0}",
+            Convert.ToBase64String(picture));
+    }
+}
