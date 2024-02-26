79a9b3b35a1ae23de7971925036234ad412903e1
lizan <xsf9728@gmail.com>
2024-02-26 09:18:05 +0800

    CppCrashCourse: chap17, fs path examples

--- Cpp/CppCrashCourse/chap17/path.cpp
+++ Cpp/CppCrashCourse/chap17/path.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 #include <filesystem>
+#include <chrono>
+
+static void describe_path(const std::filesystem::path& p)
+{
+    std::cout << std::boolalpha << "Path: " << p << std::endl;
+    try
+    {
+        std::cout << "Is directory: " << std::filesystem::is_directory(p) << std::endl;
+        std::cout << "Is regular file: " << std::filesystem::is_regular_file(p) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << '\n';
+    }
+}
+
+static void write_info(const std::filesystem::path& p)
+{
+    if (!std::filesystem::exists(p))
+    {
+        std::cout << p << " doesn't exist!" << std::endl;
+        return;
+    }
+    const auto last_write_time = std::filesystem::last_write_time(p).time_since_epoch();
+    const auto last_write_hours = std::chrono::duration_cast<std::chrono::hours>(last_write_time);
+    std::cout << p << "\tLast Modified: " << last_write_hours.count()
+        << "\tSize: " << std::filesystem::file_size(p) << std::endl;
+}
 
 int main(void)
 {
@@ -26,4 +54,41 @@ int main(void)
         << "\nChanging current dir to temp...";
     std::filesystem::current_path(temp_path);
     std::cout << "\nCurrent directory: " << std::filesystem::current_path();
+
+    std::cout << "\n --- File/directory check ---\n";
+    std::filesystem::path win_path{ R"(C:/Windows/System32/kernel32.dll)" };
+    describe_path(win_path);
+    win_path.remove_filename();
+    describe_path(win_path);
+    std::filesystem::path unix_path{ R"(/bin/bash)" };
+    describe_path(unix_path);
+    unix_path.remove_filename();
+    describe_path(unix_path);
+
+    std::cout << "\n --- File info and modificatiion ---\n";
+    unix_path = std::filesystem::path{R"(/bin/bash)"};
+    const auto readme_path = std::filesystem::temp_directory_path().append("README");
+    try
+    {
+        write_info(unix_path);
+        write_info(readme_path);
+
+        std::cout << "Copying " << unix_path.filename()
+            << " to " << readme_path << "\n";
+        std::filesystem::copy_file(unix_path, readme_path);
+        write_info(readme_path);
+
+        std::cout << "Resizing " << readme_path.filename() << "\n";
+        std::filesystem::resize_file(readme_path, 1024);
+        write_info(readme_path);
+
+        std::cout << "Removing " << readme_path.filename() << "\n";
+        std::filesystem::remove(readme_path);
+        write_info(readme_path);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << '\n';
+    }
+    
 }
