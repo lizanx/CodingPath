aaf40c0648dd5eac076019b831f588b8d5b29234
lizan <xsf9728@gmail.com>
2024-02-29 09:08:48 +0800

    CppCrashCourse: chap18, some algorithm examples

--- Cpp/CppCrashCourse/chap18/algorithms.cpp
+++ Cpp/CppCrashCourse/chap18/algorithms.cpp
@@ -186,3 +186,97 @@ TEST_CASE("search")
     REQUIRE_FALSE(itr == languages.cend());
     REQUIRE(*itr == "Go");
 }
+
+TEST_CASE("copy")
+{
+    vector<string> greeting = { "Hello", "world" };
+    vector<string> greetingCopy{};
+    copy(greeting.cbegin(), greeting.cend(), back_inserter(greetingCopy));
+    REQUIRE(greetingCopy.size() == 2);
+    REQUIRE(greetingCopy[0] == greeting[0]);
+    REQUIRE(greetingCopy[1] == greeting[1]);
+}
+
+TEST_CASE("copy_n")
+{
+    vector<string> greeting = { "Hello", "world" };
+    vector<string> greetingCopy{ "greeting" };
+    copy_n(greeting.cbegin(), greeting.size(), back_inserter(greetingCopy));
+    REQUIRE(greetingCopy.size() == 3);
+    REQUIRE(greetingCopy[0] == "greeting");
+    REQUIRE(greetingCopy[1] == greeting[0]);
+    REQUIRE(greetingCopy[2] == greeting[1]);
+}
+
+TEST_CASE("copy_backward")
+{
+    vector<string> greeting = { "Hello", "world" };
+    vector<string> greetingCopy{ "greeting", "nice", "to", "meet", "you" };
+    copy_backward(greeting.cbegin(), greeting.cend(), greetingCopy.end());
+    REQUIRE(greetingCopy.size() == 5);
+    REQUIRE(greetingCopy[0] == "greeting");
+    REQUIRE(greetingCopy[1] == "nice");
+    REQUIRE(greetingCopy[2] == "to");
+    REQUIRE(greetingCopy[3] == greeting[0]);
+    REQUIRE(greetingCopy[4] == greeting[1]);
+}
+
+TEST_CASE("move")
+{
+    struct MoveDetector
+    {
+        MoveDetector(): owner{ true } {}
+        MoveDetector(const MoveDetector&) = delete;
+        MoveDetector(MoveDetector&& other): owner{ true }
+        {
+            other.owner = false;
+        }
+        MoveDetector& operator=(const MoveDetector&) = delete;
+        MoveDetector& operator=(MoveDetector&& other)
+        {
+            owner = true;
+            other.owner = false;
+            return *this;
+        }
+
+        bool owner;
+    };
+    
+    vector<MoveDetector> detector1(2);
+    vector<MoveDetector> detector2(2);
+    move(detector1.begin(), detector1.end(), detector2.begin());
+    REQUIRE_FALSE(detector1[0].owner);
+    REQUIRE_FALSE(detector1[1].owner);
+    REQUIRE(detector2[0].owner);
+    REQUIRE(detector2[1].owner);
+}
+
+TEST_CASE("swap_ranges")
+{
+    vector<string> s1{ "The", "king", "is", "dead" };
+    vector<string> s2{ "Long", "live", "the", "king" };
+    swap_ranges(s1.begin(), s1.end(), s2.begin());
+    REQUIRE(s1 == vector<string>{ "Long", "live", "the", "king" });
+    REQUIRE(s2 == vector<string>{ "The", "king", "is", "dead" });
+}
+
+TEST_CASE("transform")
+{    
+    string greeting{ "Hello" };
+    transform(greeting.begin(), greeting.end(), greeting.begin(), ::toupper);
+    REQUIRE(greeting == "HELLO");
+
+    vector<string> greetings{ "Hello", "Hi", "Hey" };
+    vector<string> names{ "Gavin", "Echo", "Maria" };
+    vector<string> greetPeople{};
+    transform(greetings.cbegin(), greetings.cend(), names.cbegin(), back_inserter(greetPeople),
+            [](const string& greet, const string& name)
+            {
+                return greet + ", " + name + "!";
+            });
+    REQUIRE(greetPeople == vector<string>{
+        "Hello, Gavin!",
+        "Hi, Echo!",
+        "Hey, Maria!"
+    });
+}
