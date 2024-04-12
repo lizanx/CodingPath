d6b1556bafeca73e6c3c325486742eb776053f80
lizan <xsf9728@gmail.com>
2024-04-12 14:20:56 +0800

    WebDevWithBlazor: chap8, auth for BlazorServer

--- Csharp/WebDevelopmentWithBlazor/MyBlog/BlazorServer/Program.cs
+++ Csharp/WebDevelopmentWithBlazor/MyBlog/BlazorServer/Program.cs
@@ -3,6 +3,10 @@ using Data;
 using Data.Models.Interfaces;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Components.RazorComponents;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +23,13 @@ builder.Services.AddOptions<BlogApiJsonDirectAccessSetting>()
         options.TagsFolder = "Tags";
     });
 builder.Services.AddScoped<IBlogApi, BlogApiJsonDirectAccess>();
+builder.Services.AddAuth0WebAppAuthentication(
+    options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Authority"] ?? "";
+        options.ClientId = builder.Configuration["Auth0:ClientId"] ?? "";
+    });
+builder.Services.AddTransient<ILoginStatus, LoginStatus>();
 
 var app = builder.Build();
 
@@ -36,7 +47,21 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+app.MapGet("authentication/login", async (string redirectUri, HttpContext context) =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder().WithRedirectUri(redirectUri).Build();
+    await context.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+app.MapGet("authentication/logout", async (HttpContext context) =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder().WithRedirectUri("/").Build();
+    await context.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 app.Run();
