153b119a7a59b1934db304481d59bd3348e881fb
lizan <xsf9728@gmail.com>
2024-04-23 09:40:09 +0800

    BlazorInAction: chap3, add trail details view

--- Csharp/BlazorInAction/BlazingTrails/BlazingTrails.Client/Program.cs
+++ Csharp/BlazorInAction/BlazingTrails/BlazingTrails.Client/Program.cs
@@ -4,7 +4,7 @@ using BlazingTrails.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<DemoApp>("#demo-app");
+// builder.RootComponents.Add<DemoApp>("#demo-app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
