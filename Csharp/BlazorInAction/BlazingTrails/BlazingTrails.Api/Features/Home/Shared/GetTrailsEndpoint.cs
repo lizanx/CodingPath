790ed4a4c5c640b3bcf5fdbcc9c7cecf192669cd
lizan <xsf9728@gmail.com>
2024-05-01 08:35:55 +0800

    "BlazorInAction: appendix B, fix bug"

--- Csharp/BlazorInAction/BlazingTrails/BlazingTrails.Api/Features/Home/Shared/GetTrailsEndpoint.cs
+++ Csharp/BlazorInAction/BlazingTrails/BlazingTrails.Api/Features/Home/Shared/GetTrailsEndpoint.cs
@@ -7,16 +7,14 @@ namespace BlazingTrails.Api.Features.Home.Shared;
 
 public static class GetTrailsEndpoint
 {
-    public static async Task<IResult> GetTrails([FromBody] GetTrailsRequest request, BlazingTrailsContext db, CancellationToken cancellationToken = default)
+    public static async Task<IResult> GetTrails(BlazingTrailsContext db, CancellationToken cancellationToken = default)
     {
-        Console.WriteLine("GetTrails");
         var trails = await db.Trails
             .Include(t => t.Route)
             .ToListAsync(cancellationToken);
         
         var response = new GetTrailsRequest.Response(trails.Select(t =>
             new GetTrailsRequest.Trail(t.Id, t.Name, t.Image, t.Location, t.TimeInMinutes, t.Length, t.Description)));
-        Console.WriteLine("Get Trails: count = {0}", response?.Trails.Count() ?? -1);
         return Results.Ok(response);
     }
 }
