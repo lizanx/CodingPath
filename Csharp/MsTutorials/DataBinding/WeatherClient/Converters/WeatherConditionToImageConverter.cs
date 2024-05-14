7539517f2c0690548bc287cb7a9da585027bfd2a
lizan <xsf9728@gmail.com>
2024-05-14 12:04:23 +0800

    MS Tutorial: MAUI, data binding

---
+++ Csharp/MsTutorials/DataBinding/WeatherClient/Converters/WeatherConditionToImageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherClient.Models;
+
+namespace WeatherClient.Converters
+{
+    internal class WeatherConditionToImageConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type tartgetType, object? parameter, CultureInfo culture)
+        {
+            WeatherType weatherCondition = (WeatherType)value!;
+
+            return weatherCondition switch
+            {
+                Models.WeatherType.Cloudy => ImageSource.FromFile("cloud.png"),
+                Models.WeatherType.Sunny => ImageSource.FromFile("sunny.png"),
+                _ => ImageSource.FromFile("question.png")
+            };
+        }
+
+        public object? ConvertBack(object? value, Type tartgetType, object? parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ Csharp/MsTutorials/DataBinding/WeatherClient/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace WeatherClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRefresh_Clicked(object sender, EventArgs e)
+        {
+            btnRefresh.IsEnabled = false;
+            actIsBusy.IsRunning = true;
+
+            BindingContext = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
+
+            //Models.WeatherData weatherData = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
+
+            //lblWind.Text = weatherData.Wind.ToString();
+            //lblHumidity.Text = weatherData.Humidity.ToString();
+            //lblPrecipitation.Text = weatherData.Precipitation.ToString();
+            //lblTemperature.Text = weatherData.Temperature.ToString("#");
+
+            //imgCondition.Source = weatherData.Condition switch
+            //{
+            //    Models.WeatherType.Sunny => ImageSource.FromFile("sunny.png"),
+            //    Models.WeatherType.Cloudy => ImageSource.FromFile("cloud.png"),
+            //    _ => ImageSource.FromFile("question.png")
+            //};
+
+            btnRefresh.IsEnabled = true;
+            actIsBusy.IsRunning = false;
+        }
+    }
+
+}
