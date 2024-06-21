e24a439eff4e3300461d7137ada5773fb3912066
lizan <xsf9728@gmail.com>
2024-06-21 15:39:10 +0800

    BlazorWasmByExample: chap6, add weather forecast app

---
+++ Csharp/BlazorWasmByExample/Chap6/WeatherForecast/Models/OpenWeather.cs
@@ -0,0 +1,22 @@
+namespace WeatherForecast.Models;
+
+public class OpenWeather
+{
+    public Daily[] Daily { get; set; } = default!;
+}
+public class Daily
+{
+    public long Dt { get; set; }
+    public Temp Temp { get; set; } = default!;
+    public Weather[] Weather { get; set; } = default!;
+}
+public class Temp
+{
+    public double Min { get; set; }
+    public double Max { get; set; }
+}
+public class Weather
+{
+    public string Description { get; set; } = default!;
+    public string Icon { get; set; } = default!;
+}
