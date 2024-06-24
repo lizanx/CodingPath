39f4a80ad9a9b094b2e2adc9aaff80d9cefc34e3
lizan <xsf9728@gmail.com>
2024-06-24 11:01:33 +0800

    BlazorWasmByExample: chap11, finish TaskManager

--- Csharp/BlazorWasmByExample/Chap11/TaskManager/Server/Controllers/TaskItemsController.cs
+++ Csharp/BlazorWasmByExample/Chap11/TaskManager/Server/Controllers/TaskItemsController.cs
@@ -19,8 +19,60 @@ public class TaskItemsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItemAsync()
+    public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItemsAsync()
     {
         return await _db.TaskItems.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TaskItem>> GetTaskItemAsync(int id)
+    {
+        var taskItem = await _db.TaskItems.FindAsync(id);
+
+        if (taskItem == null)
+        {
+            return NotFound();
+        }
+        return taskItem;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTaskItemAsync(int id, TaskItem item)
+    {
+        var taskItem = await _db.TaskItems.FindAsync(id);
+        if (taskItem == null)
+        {
+            return NotFound();
+        }
+
+        taskItem.TaskName = item.TaskName;
+        taskItem.IsCompleted = item.IsCompleted;
+
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TaskItem>> PostTaskItemAsync(TaskItem item)
+    {
+        _db.TaskItems.Add(item);
+        await _db.SaveChangesAsync();
+        return CreatedAtAction("GetTaskItem", new { id = item.Id }, item);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTaskItemAsync(int id)
+    {
+        var taskItem = await _db.TaskItems.FindAsync(id);
+        if (taskItem == null)
+        {
+            return NotFound();
+        }
+
+        _db.TaskItems.Remove(taskItem);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
     }
 }
