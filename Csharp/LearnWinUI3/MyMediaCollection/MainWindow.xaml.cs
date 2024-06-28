858fe8b8322277491dbdb86911085abfa9437f3f
lizan <xsf9728@gmail.com>
2024-06-28 20:14:27 +0800

    Learn WinUI3: Chap2, add ListView and Filter

--- Csharp/LearnWinUI3/MyMediaCollection/MainWindow.xaml.cs
+++ Csharp/LearnWinUI3/MyMediaCollection/MainWindow.xaml.cs
@@ -25,10 +25,16 @@ namespace MyMediaCollection
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private bool _isLoaded;
+        private IList<MediaItem> _items { get; set; }
+        private IList<MediaItem> _allItems { get; set; }
+        private IList<string> _mediums { get; set; }
+
         public MainWindow()
         {
             this.InitializeComponent();
-            PopulateData();
+            ItemList.Loaded += ItemList_Loaded;
+            ItemFilter.Loaded += ItemFilter_Loaded;
         }
 
         private void PopulateData()
@@ -36,42 +42,89 @@ namespace MyMediaCollection
             if (_isLoaded) return;
             _isLoaded = true;
 
-            _items = new List<MediaItem>()
-            {
-                new () {
+            MediaItem cd = new() {
+                Id = 1,
+                Name = "Classical Favorites",
+                MediaType = ItemType.Music,
+                MediaInfo = new() {
                     Id = 1,
-                    Name = "Classical Favorites",
                     MediaType = ItemType.Music,
-                    MediaInfo = new () {
-                        Id = 1,
-                        MediaType = ItemType.Music,
-                        Name = "CD"
-                    }
-                },
-                new () {
-                    Id = 2,
-                    Name = "Classic Fairy Tales",
-                    MediaType = ItemType.Book,
-                    MediaInfo = new () {
-                        Id = 2,
-                        MediaType = ItemType.Book,
-                        Name = "Book"
-                    }
-                },
-                new () {
-                    Id = 3,
-                    Name = "The Mummy",
-                    MediaType = ItemType.Video,
-                    MediaInfo = new () {
-                        Id = 3,
-                        MediaType = ItemType.Video,
-                        Name = "Blu Ray"
-                    }
+                    Name = "CD"
                 }
             };
+            MediaItem book = new() {
+                Id = 2,
+                Name = "Classic Fairy Tales",
+                MediaType = ItemType.Book,
+                MediaInfo = new() {
+                    Id = 2,
+                    MediaType = ItemType.Book,
+                    Name = "Book"
+                }
+            };
+            MediaItem bluRay = new()
+            {
+                Id = 3,
+                Name = "The Mummy",
+                MediaType = ItemType.Video,
+                MediaInfo = new()
+                {
+                    Id = 3,
+                    MediaType = ItemType.Video,
+                    Name = "Blu Ray"
+                }
+            };
+
+            _items = [cd, book, bluRay];
+            _allItems = [cd, book, bluRay];
+
+
+            _mediums =
+            [
+                "All",
+                nameof(ItemType.Book),
+                nameof(ItemType.Music),
+                nameof(ItemType.Video),
+            ];
         }
 
-        private bool _isLoaded;
-        private IList<MediaItem> _items { get; set; }
+        private void ItemList_Loaded(object sender, RoutedEventArgs args)
+        {
+            var listView = (ListView)sender;
+            PopulateData();
+            listView.ItemsSource = _items;
+        }
+
+        private void ItemFilter_Loaded(object sender, RoutedEventArgs args)
+        {
+            var filterCombo = (ComboBox)sender;
+            PopulateData();
+            filterCombo.ItemsSource = _mediums;
+            filterCombo.SelectedIndex = 0;
+
+            ItemFilter.SelectionChanged += ItemFilter_SelectionChanged;
+        }
+
+        private void ItemFilter_SelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            var updatedItems = _allItems.Where(item =>
+                string.IsNullOrWhiteSpace(ItemFilter.SelectedValue.ToString())
+                || ItemFilter.SelectedValue.ToString() == "All"
+                || item.MediaType.ToString() == ItemFilter.SelectedValue.ToString())
+                .ToList();
+            ItemList.ItemsSource = updatedItems;
+        }
+
+        private async void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new ContentDialog
+            {
+                Title = "My Media Collection",
+                Content = "Adding items to the collection is not yet supported",
+                CloseButtonText = "OK",
+                XamlRoot = Content.XamlRoot
+            };
+            await dialog.ShowAsync();
+        }
     }
 }
