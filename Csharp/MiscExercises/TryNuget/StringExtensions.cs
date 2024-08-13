6300a8984710b2ebbaffc8bc8100549231102b39
lizan <xsf9728@gmail.com>
2024-08-13 08:07:53 +0800

    feature(C#): try dotnet pack/push to upload a classlib to nuget

---
+++ Csharp/MiscExercises/TryNuget/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace TryNuget;
+
+public static class StringExtensions
+{
+    public static string Repeat(this string str, int n)
+    {
+        if (n <= 0)
+            return string.Empty;
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < n; i++)
+        {
+            sb.Append(str);
+        }
+        return sb.ToString();
+    }
+}
