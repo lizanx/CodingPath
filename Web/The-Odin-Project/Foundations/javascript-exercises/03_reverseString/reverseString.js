3b32e3caeccaf1a1c5c99921588a03e073a2c85e
lizan <xsf9728@gmail.com>
2024-08-15 20:00:30 +0800

    feature(TOP): add answers for js-exercises 3~7

--- Web/The-Odin-Project/Foundations/javascript-exercises/03_reverseString/reverseString.js
+++ Web/The-Odin-Project/Foundations/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let chars = str.split("");
+    chars.reverse();
+    return chars.join("");
 };
 
 // Do not edit below this line
--- Web/The-Odin-Project/Foundations/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ Web/The-Odin-Project/Foundations/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, element) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== element) {
+            newArray.push(arr[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
