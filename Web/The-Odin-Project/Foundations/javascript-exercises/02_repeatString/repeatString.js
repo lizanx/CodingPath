498db8e908798ab789a42e9aa05b83d40fc67162
lizan <xsf9728@gmail.com>
2024-08-15 18:57:04 +0800

    feature(TOP): add solutions to js-exercises 1&2

--- Web/The-Odin-Project/Foundations/javascript-exercises/02_repeatString/repeatString.js
+++ Web/The-Odin-Project/Foundations/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, count) {
+    let repeatedStr = "";
+    for (let i = 0; i < count; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
