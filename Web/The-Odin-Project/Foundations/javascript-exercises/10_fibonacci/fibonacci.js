feb98c18eff9d3edbf644067ca8336abf4f5dfb2
lizan <xsf9728@gmail.com>
2024-08-20 14:00:21 +0800

    feature(TOP): add answers for TOP js-exercises

--- Web/The-Odin-Project/Foundations/javascript-exercises/10_fibonacci/fibonacci.js
+++ Web/The-Odin-Project/Foundations/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (typeof index !== 'number') {
+        index = parseInt(index, 10);
+    }
+    if (index < 0) {
+        return "OOPS";
+    }
+    if (index === 0) {
+        return 0;
+    }
+    if (index === 1 || index === 2) {
+        return 1;
+    }
+    return fibonacci(index - 2) + fibonacci(index - 1);
 };
 
 // Do not edit below this line
--- Web/The-Odin-Project/Foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ Web/The-Odin-Project/Foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((accumulator, person) => {
+        return getAge(accumulator) > getAge(person) ? accumulator : person;
+    });
 
+    function getAge(person) {
+        return (person.yearOfDeath ? person.yearOfDeath : (new Date()).getFullYear()) - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
