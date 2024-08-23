34802441d9691966b7a052d0905122dfd2991d60
lizan <xsf9728@gmail.com>
2024-08-23 15:39:30 +0800

    add(C#): test getting environment information using Environment

--- Csharp/MiscExercises/TryEnvGetCmdArgs/Program.cs
+++ Csharp/MiscExercises/TryEnvGetCmdArgs/Program.cs
@@ -14,3 +14,25 @@ foreach (string arg in envArgs)
 {
     Console.WriteLine(arg);
 }
+
+Console.WriteLine();
+
+Console.WriteLine("### Show information you can get via Environment class ###");
+ShowEnvironmentDetails();
+
+static void ShowEnvironmentDetails()
+{
+    Console.WriteLine("OS Version: {0}", Environment.OSVersion);
+    Console.WriteLine(".NET Version: {0}", Environment.Version);
+    foreach (string drive in Environment.GetLogicalDrives())
+    {
+        Console.WriteLine("Drive: {0}", drive);
+    }
+    Console.WriteLine("Is 64-bit OS: {0}", Environment.Is64BitOperatingSystem);
+    Console.WriteLine("Is 64-bit process: {0}", Environment.Is64BitProcess);
+    Console.WriteLine("Number of CPU cores: {0}", Environment.ProcessorCount);
+    Console.WriteLine("Machine name: {0}", Environment.MachineName);
+    Console.WriteLine("User name: {0}", Environment.UserName);
+    Console.WriteLine("Current process ID: {0}", Environment.ProcessId);
+    Console.WriteLine("System dir path: {0}", Environment.SystemDirectory);
+}
