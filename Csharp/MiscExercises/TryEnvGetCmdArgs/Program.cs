1f319a4e261e97c16a2f861faab1eb27f6aa32ae
lizan <xsf9728@gmail.com>
2024-08-23 15:23:54 +0800

    add(C#): test diffs between 'args' and 'Environment.GetCommandLineArgs()'

---
+++ Csharp/MiscExercises/TryEnvGetCmdArgs/Program.cs
@@ -0,0 +1,16 @@
+﻿// args doesn't contain extra arguments other passed in terminal.
+Console.WriteLine("### Command line arguments from parameter of Main ###");
+foreach (string arg in args)
+{
+    Console.WriteLine(arg);
+}
+
+Console.WriteLine();
+
+// envArgs contains an extra argument for program name(may include path) as first argument
+Console.WriteLine("### Command line arguments from Env.GetCmdLineArgs ###");
+var envArgs = Environment.GetCommandLineArgs();
+foreach (string arg in envArgs)
+{
+    Console.WriteLine(arg);
+}
