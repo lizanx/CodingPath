c4f7427a3edc447782ef8c4f2378cf6ef202d187
lizan <xsf9728@gmail.com>
2024-08-28 08:39:57 +0800

    add(C#): add array/enum examples for chap4 of 'Pro C# 10'

---
+++ Csharp/ProC#10With.NET6/chap4/ArrayExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿var arr = new[] { "C#", "C++", "C", "Go", "Zig" };
+
+System.Console.WriteLine("First 2:");
+PrintArray(arr[0..2]);
+PrintArray(arr[..2]);
+
+System.Console.WriteLine("Last 3:");
+PrintArray(arr[^3..^0]);
+PrintArray(arr[^3..]);
+
+Range middle = 1..^1;
+System.Console.WriteLine("Middle(exclude first one and last one):");
+PrintArray(arr[middle]);
+
+Array.Reverse(arr);
+System.Console.WriteLine("After reverse:");
+PrintArray(arr);
+
+Array.Clear(arr);
+System.Console.WriteLine("After clear:");
+PrintArray(arr);
+
+static void PrintArray<T>(T[] arr)
+{
+    foreach (T item in arr)
+    {
+        Console.WriteLine(item + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Csharp/ProC#10With.NET6/chap4/EnumExamples/Program.cs
@@ -0,0 +1,35 @@
+﻿ColorEnum green = ColorEnum.Green;
+Console.WriteLine(
+    "Type name: {0}, underlying type: {1}, value: {2}",
+    green.GetType().Name,
+    Enum.GetUnderlyingType(green.GetType()).Name,
+    (int)green
+);
+
+Console.WriteLine("{0}All colors:", Environment.NewLine);
+foreach (var color in Enum.GetValues<ColorEnum>())
+{
+    Console.WriteLine(color);
+}
+Console.WriteLine();
+
+var edible = FoodEnum.Eatable | FoodEnum.Drinkable;
+Console.WriteLine(
+    "Edible is {0} and {1}",
+    (edible | FoodEnum.Eatable) == edible ? "eatable" : "non-eatable",
+    (edible | FoodEnum.Drinkable) == edible ? "drinkable" : "non-drinkable"
+);
+
+enum ColorEnum
+{
+    Red,
+    Green,
+    Blue
+}
+
+[Flags]
+enum FoodEnum
+{
+    Eatable = 0b_1,
+    Drinkable = 0b_10
+}
