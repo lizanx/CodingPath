ac9886d891c00ea61e844e75620f677034ad220f
lizan <xsf9728@gmail.com>
2024-08-29 09:28:27 +0800

    add(C#): try Canvas control in Avalonia

---
+++ Csharp/Avalonia/GetStartedApp/CanvasWindow.axaml.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace GetStartedApp;
+
+public partial class CanvasWindow : Window
+{
+    public CanvasWindow()
+    {
+        InitializeComponent();
+    }
+}
