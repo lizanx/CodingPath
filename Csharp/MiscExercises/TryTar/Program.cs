b788ea9cba39437a0736a0cefa71ffe8ab996ba9
lizan <xsf9728@gmail.com>
2024-09-06 08:25:35 +0800

    add(C#): try looping through tar entries and extracting a single file

--- Csharp/MiscExercises/TryTar/Program.cs
+++ Csharp/MiscExercises/TryTar/Program.cs
@@ -22,3 +22,38 @@ using (var fs = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
         await TarFile.ExtractToDirectoryAsync(gs, dstDir, overwriteFiles: true);
     }
 }
+
+// Loop through each entry in the tar file without extraction.
+// Extract a single file in tar.gz
+string dstSingleFile = "./Tar/Single/Cars.json";
+using (var fs = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
+{
+    using (var gs = new GZipStream(fs, CompressionMode.Decompress))
+    {
+        using (var reader = new TarReader(gs, leaveOpen: true))
+        {
+            while (reader.GetNextEntry() is TarEntry entry)
+            {
+                Console.WriteLine(
+                    "[TarEntry] Name: {0}, Length: {1}, "
+                        + "ModificationTime: {2}, Checksum: {3}"
+                        + "\n  Mode: {4}",
+                    entry.Name,
+                    entry.Length,
+                    entry.ModificationTime,
+                    entry.Checksum,
+                    entry.Mode
+                );
+                if (Path.GetFileName(entry.Name) == "Cars.json")
+                {
+                    Console.WriteLine(
+                        "Found matched single file {0} in {1}",
+                        entry.Name,
+                        sourceFile
+                    );
+                    entry.ExtractToFile(dstSingleFile, overwrite: true);
+                }
+            }
+        }
+    }
+}
