1ec77fc21d2d83e843443c8f444a0e3e71015505
lizan <xsf9728@gmail.com>
2024-09-27 09:19:20 +0800

    add(C#): try bundling embedded resource files in .NET

---
+++ Csharp/MiscExercises/TryBundlingResourceFiles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+Console.WriteLine("Demo Start");
+
+// Console.WriteLine("App data: {0}", File.ReadAllText("app_data.txt"));
+
+var assembly = Assembly.GetEntryAssembly()!;
+using var appDataStream = assembly.GetManifestResourceStream(
+    $"{assembly.GetName().Name}.app_data.txt"
+)!;
+using var reader = new StreamReader(appDataStream);
+Console.WriteLine("App data: {0}", reader.ReadToEnd());
+
+Console.WriteLine("Demo end");
