b17167cc15128cdc6691d5ad16b026357e7d5574
lizan <xsf9728@gmail.com>
2024-11-01 10:11:20 +0800

    add(C#): try listening to ObservableCollection's CollectionChanged event

---
+++ Csharp/ProC#10With.NET6/chap10/ObservableCollectionExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+ObservableCollection<Person> people = new ObservableCollection<Person>
+{
+    new("Alice", 22),
+    new("Bob", 17),
+    new("Chris", 35)
+};
+
+people.CollectionChanged += PeopleChanged;
+people.Add(new Person("Jane", 25));
+people.RemoveAt(1);
+people.Add(new Person("John", 55));
+people.RemoveAt(0);
+people.CollectionChanged -= PeopleChanged;
+
+void PeopleChanged(object? sender, NotifyCollectionChangedEventArgs e)
+{
+    Console.WriteLine("People changed! Action: {0}", e.Action);
+
+    if (e.Action == NotifyCollectionChangedAction.Add)
+    {
+        foreach (var person in e.NewItems!)
+        {
+            Console.WriteLine("\tAdded person: {0}", person);
+        }
+    }
+    else if (e.Action == NotifyCollectionChangedAction.Remove)
+    {
+        foreach (var person in e.OldItems!)
+        {
+            Console.WriteLine("\tRemoved person: {0}", person);
+        }
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Person(Name: {Name}, Age: {Age})";
+    }
+}
