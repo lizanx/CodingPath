7a9f7c8ca9e3806e18d6933bee695b63731bd258
Gavin <xsf9728@gmail.com>
2025-04-29 16:48:54 +0800

    add(CMake): update config for module test

--- Tools/CMake/ModernCMakeForC++/chap10/cxx_modules/main.cpp
+++ Tools/CMake/ModernCMakeForC++/chap10/cxx_modules/main.cpp
@@ -6,7 +6,8 @@ int main()
 {
     int a{7}, b{3};
 
-    std::cout << "a + b = " << add(a, b) << "\n"
+    std::cout << "a = " << a << ", b = " << b << "\n"
+              << "a + b = " << add(a, b) << "\n"
               << "a - b = " << sub(a, b) << "\n"
               << "a * b = " << multiply(a, b) << "\n"
               << "a / b = " << divide(a, b) << "\n";
