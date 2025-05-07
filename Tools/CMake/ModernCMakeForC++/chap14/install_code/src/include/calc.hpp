ca3d80ac24ac497f5b1e058748a8c612a2986d31
Gavin <xsf9728@gmail.com>
2025-05-07 15:56:13 +0800

    add(CMake): try install code/export/config

---
+++ Tools/CMake/ModernCMakeForC++/chap14/install_code/src/include/calc.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+class Calculator
+{
+public:
+    int Sum(int a, int b);
+    int Multiply(int a, int b);
+};
---
+++ Tools/CMake/ModernCMakeForC++/chap14/install_export/calc/src/calc.cpp
@@ -0,0 +1,11 @@
+#include "calc.hpp"
+
+int Calculator::Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Multiply(int a, int b)
+{
+    return a * b;
+}
