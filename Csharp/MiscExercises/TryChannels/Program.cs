f6f4854e20967618379fc9a7bef6e815f0908495
lizan <xsf9728@gmail.com>
2025-08-12 14:00:33 +0800

    add(C#): try channels for producer/consumer

---
+++ Csharp/MiscExercises/TryChannels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+var channel = Channel.CreateUnbounded<string>();
+
+_ = Task.Run(async () =>
+{
+    for (var i = 0; i < 10; ++i)
+    {
+        await channel.Writer.WriteAsync($"Message[{i}]");
+        Console.WriteLine("Produced: {0}", i);
+        await Task.Delay(200);
+    }
+
+    channel.Writer.Complete();
+});
+
+await foreach (var msg in channel.Reader.ReadAllAsync())
+{
+    Console.WriteLine("Received: {0}", msg);
+    await Task.Delay(250);
+}
+
+Console.WriteLine("Processing complete.");
