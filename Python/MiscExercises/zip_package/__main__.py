d877b2896922d26486f6883d2a08245f828f8518
lizan <xsf9728@gmail.com>
2025-08-18 23:17:24 +0800

    add(Python): try packaging python app as a zip package

---
+++ Python/MiscExercises/zip_package/__main__.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    print("Hello world!")
+
+# zip pkg.zip __main__.py
+# python pkg.zip
+
+# or
+
+# zip pkg.zip __main__.py
+# echo '#!/usr/bin/env python' > pkg
+# cat pkg.zip >> pkg
+# chmod +x pkg
+# ./pkg
