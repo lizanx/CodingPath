829b17e5e0b2d45225d3cb49b07866a9363315ca
lizan <xsf9728@gmail.com>
2026-02-10 14:51:34 +0800

    add(C#): try non-buffer bounded channel

---
+++ Csharp/MiscExercises/TryNoBufBoundedChannel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+
+var channel = Channel.CreateBounded<int>(0);
+
+var producer = Task.Run(async () =>
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        Console.WriteLine($"[Producer] Producing data {i} - {DateTimeOffset.Now}");
+        await channel.Writer.WriteAsync(i);
+        Console.WriteLine($"[Producer] Produced data {i} - {DateTimeOffset.Now}");
+    }
+    channel.Writer.Complete();
+});
+
+var consumer = Task.Run(async () =>
+{
+    await foreach (int i in channel.Reader.ReadAllAsync())
+    {
+        Console.WriteLine($"[Consumer] Consuming data {i} - {DateTimeOffset.Now}");
+        await Task.Delay(500);
+        Console.WriteLine($"[Consumer] Consumed data {i} - {DateTimeOffset.Now}");
+    }
+});
+
+await Task.WhenAll(producer, consumer);
+
+Console.WriteLine("=== All Done ===");
